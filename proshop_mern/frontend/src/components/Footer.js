aacc4fbc84d8876ae28f4062c8502bcbbb5d9b21
Jaweed Khan <mr.jaweedkhan@gmail.com>
2021-12-29 16:02:23 +0530

    Header, Footer & React Bootstrap

---
+++ proshop_mern/frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col ClassName="text-center py-3">
+                        Copyright &copy; {new Date().getFullYear()} ProShop
+                    </Col>
+                </Row>
+            
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
